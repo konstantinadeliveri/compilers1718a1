82c7b93da212c4415ece036e149a9f1c8b60ec68
konstantinadeliveri <p15deli@ionio.gr>
2018-03-20 05:46:30 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,4 +1,3 @@
-
 def getchar(words,pos):
 	""" returns char at pos of words, or None if out of bounds """
 
@@ -38,29 +37,26 @@ def scan(text,transition_table,accept_states):
 	
 # the transition table, as a dictionary
 
-# Αντικαταστήστε με το δικό σας λεξικό μεταβάσεων...
-td = { 'q0':{ 't':'q1','l':'q2' },
-       'q1':{ 'e':'q3' },
-       'q2':{ 'o':'q8' },
-       'q3':{ 's':'q4','r':'q6' },
-       'q4':{ 't':'q5' },
-       'q6':{ 'm':'q7' },
-       'q8':{ 'n':'q9' },
-       'q9':{ 'g':'q10'}
+# Το νέο λεξικό μεταβάσεων 
+td = { "q0":{ "0":"q1","1":"q1","2":"q2","3":"q3","4":"q3","5":"q3","6":"q3","7":"q3","8":"q3","9":"q3" },
+       "q1":{ "0":"q4","1":"q4","2":"q4","3":"q4","4":"q4","5":"q4","6":"q4","7":"q4","8":"q4","9":"q4",".":"q6",":":"q6" },
+       "q2":{ "0":"q5","1":"q5","2":"q5","3":"q5",".":"q6",":":"q6" },
+       "q3":{ ".":"q6",":":"q6" },  
+       "q4":{ ".":"q6",":":"q6" },
+       "q5":{ ".":"q6",":":"q6" },
+       "q6":{ "0":"q7","1":"q7","2":"q7","3":"q7","4":"q7","5":"q7" },
+       "q7":{ "0":"q8","1":"q8","2":"q8","3":"q8","4":"q8","5":"q8","6":"q8","7":"q8","8":"q8","9":"q8" }	
      } 
 
 # the dictionary of accepting states and their
 # corresponding token
 
-# Αντικαταστήστε με το δικό σας λεξικό καταστάσεων αποδοχής...
-ad = { 'q5':'TEST_TOKEN',
-       'q7':'TERM_TOKEN',
-       'q10':'LONG_TOKEN'
-     }
+# Το νέο λεξικό καταστάσεων αποδοχής
+ad = { "q8":'TIME_TOKEN' }
 
 
 # get a string from input
-text = input('give some input>')
+text = input("Give Time:>")
 
 # scan text until no more input
 while text:	# that is, while len(text)>0
@@ -75,5 +71,5 @@ while text:	# that is, while len(text)>0
 	print("token:",token,"string:",text[:position])
 	
 	# remaining text for next scan
-	text = text[position:]
+text = text[position:]
 	
